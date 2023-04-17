ee6cc3d9a908a92e2e4b2ced4c5dba7ec94b8cb2
harsh <harsh.goswami3054@gmail.com>
2023-04-17 16:16:47 +0530

    changed index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use("/posts", postRoutes);
+app.use("/", postRoutes);
 
 const CONNECTION_URL = process.env.MONGO_URI;
 const PORT = process.env.PORT || 5000;
