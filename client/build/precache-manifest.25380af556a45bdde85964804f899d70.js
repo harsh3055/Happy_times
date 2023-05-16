7fab60c40f99d24319026ca8f765ca856a8efa20
harsh <harsh.goswami3054@gmail.com>
2023-05-17 00:29:11 +0530

    🤢 trying new emojis in commit

---
+++ client/build/precache-manifest.25380af556a45bdde85964804f899d70.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6c5dc484d3cf14c3c8517d0a019a10c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5aa5ac5d23957a2aee4",
+    "url": "/static/css/main.c7aedc99.chunk.css"
+  },
+  {
+    "revision": "f67d104b1c2aae719d62",
+    "url": "/static/js/2.abbed91d.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/static/js/2.abbed91d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5aa5ac5d23957a2aee4",
+    "url": "/static/js/main.77314e54.chunk.js"
+  },
+  {
+    "revision": "f620b634b443fc7d2dfb",
+    "url": "/static/js/runtime-main.7e6e88a6.js"
+  },
+  {
+    "revision": "35351cc30ad00c1ff4f3df950040f43b",
+    "url": "/static/media/exposed.35351cc3.jpg"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
